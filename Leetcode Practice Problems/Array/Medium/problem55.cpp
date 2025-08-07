2e1fa0eadf1bb7fe4b58917e4b3dd18aa9e3f5ce
Shaurya Gupta <prakhargupta9935@gmail.com>
2025-08-07 23:58:09 +0530

    Problem 55

---
+++ Leetcode Practice Problems/Array/Medium/problem55.cpp
@@ -0,0 +1,17 @@
+// This is Problem 55 on leetcode 
+// I came up with follwing solution
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+     int reachable = 0;  
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > reachable) return false;  
+        reachable = max(reachable, i + nums[i]);
+    }
+    return true;   
+    }
+};
